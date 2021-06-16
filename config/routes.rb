f55982f0a35b6f5a50a96a62f2e0516c3bdb3992
popo03 <83389638+popo03@users.noreply.github.com>
2021-06-16 11:18:58 +0900

    コメント機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "groups#index"
   resources :users, only: [:index, :new, :create, :edit, :update]
-  resources :groups, only: [:index, :new, :create] do
+  resources :groups, only: [:index, :new, :create, :destroy] do
     resources :rooms, only:[:index, :create]
   end
 end
